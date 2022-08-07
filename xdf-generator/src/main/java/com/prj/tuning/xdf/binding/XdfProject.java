3907d934f98b3d1a02357c7dcbd243ac3086b7a2
Jerome M <marcjero@yahoo.com>
2022-08-08 01:35:17 +0200

    XDF tags ordering

--- xdf-generator/src/main/java/com/prj/tuning/xdf/binding/XdfProject.java
+++ xdf-generator/src/main/java/com/prj/tuning/xdf/binding/XdfProject.java
@@ -8,7 +8,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "XDFFORMAT")
 @XmlType(propOrder={"XDFHEADER", "XDFTABLE", "XDFCONSTANT"})
-
 public abstract class XdfProject {
   public static final String ADDRESS_FORMAT = "0x%X";
 
