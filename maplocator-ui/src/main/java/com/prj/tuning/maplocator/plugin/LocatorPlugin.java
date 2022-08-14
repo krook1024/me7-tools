fd8adff18a4616f2c886a1a91d1079baf4627d26
Jerome M <marcjero@yahoo.com>
2022-08-14 03:07:59 +0200

    maplocator-plugin

--- maplocator-ui/src/main/java/com/prj/tuning/maplocator/plugin/LocatorPlugin.java
+++
@@ -1,9 +0,0 @@
-package com.prj.tuning.maplocator.plugin;
-
-import java.util.Collection;
-
-import com.prj.tuning.maplocator.model.LocatedMap;
-
-public interface LocatorPlugin {
-  Collection<? extends LocatedMap> locateMaps(byte[] binary);
-}
